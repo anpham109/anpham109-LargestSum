0f0269374cd77b5e2d248cfd1a64dc9f4a850199
Andy Pham <anpham-work@protonmail.com>
2023-08-18 14:38:51 +0000

    finished the challenge

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -10,6 +10,14 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        int biggestInt = nums.get(0);
+        int secondBiggestInt = nums.get(0);
+        for(int i = 0; i < nums.size(); i++){
+            if(nums.get(i) > biggestInt){
+                secondBiggestInt = biggestInt;
+                biggestInt = nums.get(i);
+            }
+        }
+        return biggestInt + secondBiggestInt;
     }
 }
